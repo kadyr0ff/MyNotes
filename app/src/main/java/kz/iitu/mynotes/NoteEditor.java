5958c1a464e6312abd0d4ee6a11273e7f910d6b2
alisherkadyrov <kadyr0ff@outlook.com>
2021-05-14 09:33:28 +0600

    Added Save & Delete buttons to NoteEditor.java

--- app/src/main/java/kz/iitu/mynotes/NoteEditor.java
+++ app/src/main/java/kz/iitu/mynotes/NoteEditor.java
@@ -1,13 +1,16 @@
 package kz.iitu.mynotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.HashSet;
@@ -15,6 +18,34 @@ import java.util.HashSet;
 public class NoteEditor extends AppCompatActivity {
 
     int noteId;
+
+    public void onClickNoteSave(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickNoteDelete(View view){
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you want to delete this note?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.notes.remove(noteId);
+                        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+
+                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("kz.iitu.mynotes", Context.MODE_PRIVATE);
+                        HashSet<String> set = new HashSet<>(MainActivity.notes);
+                        sharedPreferences.edit().putStringSet("notes", set).apply();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                }).setNegativeButton("No", null).show();
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
